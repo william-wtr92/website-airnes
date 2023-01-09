e2210b1ff5067acd792ef02a16e2b52e3ed9535d
William <wwautrin@hotmail.fr>
2023-01-09 11:20:15 +0100

    Add Responsive

--- src/components/FooterMenu.jsx
+++ src/components/FooterMenu.jsx
@@ -10,12 +10,24 @@ const FooterMenu = () => {
   return (
     <footer>
       <div className="flex bg-[#709861] h-10 justify-start absolute bottom-0 w-full">
-        <div className="flex mr-auto ml-4 mt-2 gap-4 text-white font-ligth">
-          <NavLink href="/" >CGU</NavLink>
-          <NavLink href="/">Mentions Légales</NavLink>
-          <NavLink href="/">Contact</NavLink>
+        <div className="FooterMenu flex mr-auto ml-4 mt-2 gap-4 text-white font-ligth">
+          <NavLink href="/" >
+            <div className="hover:text-[#97c186] hover:scale-110">
+             CGU 
+            </div>
+          </NavLink>
+          <NavLink href="/">
+            <div className="hover:text-[#97c186] hover:scale-110">
+             Mentions légales
+            </div>
+          </NavLink>
+          <NavLink href="/">
+            <div className="hover:text-[#97c186] hover:scale-110">
+             Contact
+            </div>
+          </NavLink>
         </div>
-        <div className="flex gap-6 mr-10 mt-2">
+        <div className="FooterLink flex gap-6 mr-10 mt-2">
           <NavLink href="/">
             <FontAwesomeIcon icon={faLinkedin} className="h-6 text-white hover:scale-110"/>
           </NavLink>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,27 @@
 import NavMenu from "@/components/NavMenu"
 import FooterMenu from "@/components/FooterMenu"
 import CarouselMain from "@/components/Carousel"
+import Head from "next/head"
 
 const Main = () => {
   return (
     <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
+          rel="stylesheet"
+        />
+        <title>Airnes</title>
+      </Head>
       <NavMenu />
       <main>
         <CarouselMain />
+        <div className="MainText flex flex-col text-xl font-bold items-center justify-center cursor-pointer">
+          <p>VENANT DES HAUTES TERRES D’ECOSSE NOS</p>
+          <p>MEUBLES SONT IMMORTELS</p>
+        </div>
       </main>
       <FooterMenu />
     </>
