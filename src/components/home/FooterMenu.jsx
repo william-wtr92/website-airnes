1a7fa9b5e8f882bb229bb09cee7f2de3548fb04d
William <wwautrin@hotmail.fr>
2023-02-02 15:44:01 +0100

    Revert "Merge pull request #20 from william-wtr92/SDV-68-all-fix"
    This reverts commit 1a5c3002e946356c63639449328313c5da8b679d, reversing
    changes made to 8009e37d1d412a1cb2ac3e95dedd0a4c09068ad9.

--- src/components/home/FooterMenu.jsx
+++ src/components/home/FooterMenu.jsx
@@ -10,45 +10,43 @@ const FooterMenu = (props) => {
   return (
     <footer>
       <div
-        className={`flex justify-center bg-[#ffffff] text-[#615043] h-14 border-t-2 border-[#615043] ${props.position} bottom-0 w-full`}
+        className={`flex bg-[#ffffff] text-[#615043] h-14 border-t-2 border-[#615043] ${props.position} bottom-0 w-full`}
       >
-        <div className="hidden lg:block">
-          <div className="flex items-center my-4 font-semibold ml-10 gap-10">
-            <NavLink href="/">
-              <div className="text-[12px] hover:text-[#b3825c] hover:scale-110 text-sm">
-                CGU
-              </div>
-            </NavLink>
-            <NavLink href="/">
-              <div className="text-[12px] hover:text-[#b3825c] hover:scale-110 text-sm">
-                Mentions légales
-              </div>
-            </NavLink>
-            <NavLink href="/">
-              <div className="text-[12px] hover:text-[#b3825c] hover:scale-110 text-sm">
-                Contact
-              </div>
-            </NavLink>
-          </div>
+        <div className="flex ml-2 items-center gap-2 font-semibold lg:ml-10 lg:gap-10">
+          <NavLink href="/">
+            <div className="text-[12px] hover:text-[#b3825c] hover:scale-110 lg:text-sm">
+              CGU
+            </div>
+          </NavLink>
+          <NavLink href="/">
+            <div className="text-[12px] hover:text-[#b3825c] hover:scale-110 lg:text-sm">
+              Mentions légales
+            </div>
+          </NavLink>
+          <NavLink href="/">
+            <div className="text-[12px] hover:text-[#b3825c] hover:scale-110 lg:text-sm">
+              Contact
+            </div>
+          </NavLink>
         </div>
 
-        <div className="flex items-center gap-4 lg:ml-auto lg:mx-0 lg:mr-16 lg:gap-10">
+        <div className="flex items-center gap-4 ml-auto mr-5 lg:mr-16 lg:gap-10">
           <NavLink href="/">
             <FontAwesomeIcon
               icon={faLinkedin}
-              className="h-6 text-primary hover:scale-110"
+              className="h-6 text-white hover:scale-110"
             />
           </NavLink>
           <NavLink href="/">
             <FontAwesomeIcon
               icon={faInstagram}
-              className="h-6 text-primary hover:scale-110"
+              className="h-6 text-white hover:scale-110"
             />
           </NavLink>
           <NavLink href="/">
             <FontAwesomeIcon
               icon={faFacebookF}
-              className="h-6 text-primary hover:scale-110"
+              className="h-6 text-white hover:scale-110"
             />
           </NavLink>
         </div>
--- src/components/home/NavMenu.jsx
+++ src/components/home/NavMenu.jsx
@@ -4,11 +4,9 @@ import {
   UserIcon,
   MagnifyingGlassIcon,
   Bars3Icon,
-  XMarkIcon,
 } from "@heroicons/react/24/solid"
 import { useState } from "react"
 import { useMediaQuery } from "react-responsive"
-import Image from "next/image"
 
 const NavMenu = () => {
   const [showSearch, setShowSearch] = useState(false)
@@ -30,15 +28,13 @@ const NavMenu = () => {
 
   return (
     <header className="bg-[#ffffff] sticky top-0 z-20">
-      <div className="flex h-14 items-center shadow-sm shadow-[#615043]">
-        <div className="flex mr-auto ml-2 mt-2 lg:ml-10">
+      <div className="flex h-14 border-b-2 items-center shadow-xl shadow-[#615043]">
+        <div className="flex mr-auto ml-2 lg:ml-10">
           <NavLink href="/">
-            <Image
+            <img
               src="/images/logo.png"
-              alt="Logo"
-              width={100}
-              height={1}
-              className="h-24 w-24 hover:scale-110 lg:w-24"
+              alt="My logo"
+              className="h-20 ml-2 hover:scale-110 lg:ml-0 lg:h-24"
             />
           </NavLink>
         </div>
@@ -57,11 +53,11 @@ const NavMenu = () => {
               <div className="flex items-center">
                 <MagnifyingGlassIcon
                   onClick={handleSearch}
-                  className="h-4 hover:scale-110 text-[#443021] relative top-px cursor-pointer left-5 z-20"
+                  className="h-4 hover:scale-110 text-[#443021] relative top-px cursor-pointer left-2 z-20 lg:left-1"
                   color={"#615043"}
                 />
                 <input
-                  className={`px-6 rounded-lg border bg-transparent focus:outline-none focus:shadow-outline-blue text-white placeholder-[#443021] focus:border-white ${
+                  className={`px-6 rounded-lg border bg-transparent focus:outline-none focus:shadow-outline-blue text-white placeholder-[#443021] -ml-4 focus:border-white ${
                     isMobile ? `z-10` : ``
                   }`}
                   type="search"
@@ -80,7 +76,7 @@ const NavMenu = () => {
               />
             </div>
           )}
-          <NavLink href="/user/userId/cart">
+          <NavLink href="/user/userId/cart/cart">
             <ShoppingCartIcon
               className={`${
                 showSearch && isMobile ? `Hide` : ``
@@ -101,24 +97,18 @@ const NavMenu = () => {
         <div
           className={`${
             burgerMenu ? `block ` : `hidden `
-          }w-1/6 h-screen bg-[#ffffffde] absolute inset-y-0 right-0 z-50`}
+          }w-1/6 h-screen bg-[#646E4E] absolute inset-y-0 right-0 -z-50`}
         >
-          <div>
-            <XMarkIcon
-              className="h-6 hover:cursor-pointer relative top-4 left-3 hover:scale-105"
-              onClick={showBurgerMenu}
-            />
-          </div>
-          <div className="flex flex-col mx-16 my-10">
-            <NavLink href="/user/login">
-              <div className="hover:text-[#615043] mb-10 hover:scale-105">
-                Login
-              </div>
-            </NavLink>
-            <NavLink href="/">
-              <div className="hover:text-[#615043] hover:scale-105">Home</div>
-            </NavLink>
-          </div>
+          <NavLink href="/user/login">
+            <div className="hover:text-[#97c186] p-10 h-10 hover:scale-110">
+              Login
+            </div>
+          </NavLink>
+          <NavLink href="/">
+            <div className="hover:text-[#97c186] p-10 h-10 hover:scale-110">
+              Home
+            </div>
+          </NavLink>
         </div>
       </div>
     </header>
