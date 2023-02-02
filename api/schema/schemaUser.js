2db21ffdc983d387eb1ff8024dd16406cc42dd21
William <wwautrin@hotmail.fr>
2023-02-02 21:16:54 +0100

    Change db connection

--- api/schema/schemaUser.js
+++ api/schema/schemaUser.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose")
 const { Schema } = mongoose
+const connection = require("../dbConnect")
 
 const UserSchema = new Schema({
   _id: Number,
   name: { type: String, required: true },
 })
 
-const User = mongoose.model("User", UserSchema)
+const User = connection.model("User", UserSchema)
 
 module.exports = User
