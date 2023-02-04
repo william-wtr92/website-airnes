0b5f2918a4ba18a3db06d32abe24f8fcdfb50563
Charlotte <charlotte.waegeneire@hotmail.fr>
2023-02-04 16:12:31 +0100

    Updated the initial values using useEffect but the initialValues are not re-rendering... - needs to be fixed

--- src/pages/payment/checkout.jsx
+++ src/pages/payment/checkout.jsx
@@ -2,12 +2,11 @@ import * as yup from "yup"
 import {Form, Formik} from "formik"
 import FormField from "@/components/utils/FormField"
 import Button from "@/components/utils/Button"
-import {NavLink} from "@/components/utils/NavLink"
-import React, {useState} from "react"
+import React, {useEffect, useState} from "react"
 
 const exampleAddresses = [
     {
-        id: 0,
+        id: "1",
         name: "Domicile",
         address: "19 rue de la Lune",
         complete: "batiment 21",
@@ -15,7 +14,7 @@ const exampleAddresses = [
     }
     ,
     {
-        id: 1,
+        id: "2",
         name: "Maison parents",
         address: "69 rue de la Lune",
         complete: "batiment 07",
@@ -24,45 +23,49 @@ const exampleAddresses = [
 ]
 
 const defaultValidationSchema = yup.object().shape({
-    /* Ajouter les règles du select */
-    firstName: yup.string()
-        .required()
-        .label("Prénom"),
-    name: yup.string()
-        .required()
-        .label("Nom"),
+    firstName: yup.string().required().label("Prénom"),
+    name: yup.string().required().label("Nom"),
     /* Faire les règles du select */
-    address: yup.string()
-        .required()
-        .label("Adresse"),
-    complete: yup.number()
-        .label("Complément d'adresse"),
-    city: yup.string()
-        .required()
-        .label("Ville")
-    
+    address: yup.string().required().label("Adresse"),
+    complete: yup.string().label("Complément d'adresse"),
+    city: yup.string().required().label("Ville"),
 })
 
 const defaultInitialValues = {
-    firstName: "",
-    name: "",
-    address: "",
-    complete: "",
-    city: "",
+    firstName: "John",
+    name: "Doe",
+    chosen: {
+        address: "",
+        complete: "",
+        city: "",
+    }
 }
 
 const Payment = (props) => {
     const [selectedId, setSelectedId] = useState(0)
-
-    const handleInput = (event) => {
-        setSelectedId(event.target.options[event.target.selectedIndex].id)
-    }
+    const [formValues, setFormValues] = useState(defaultInitialValues)
 
     const {
         onSubmit,
-        initialValues = defaultInitialValues,
+        initialValues = formValues,
         validationSchema = defaultValidationSchema,
     } = props
+
+    const getCardId = (event) => {
+        setSelectedId(event.target.options[event.target.selectedIndex].id)
+    }
+
+    useEffect(() => {
+        setFormValues(prevValues => ({
+            ...prevValues,
+            chosen: selectedId ?
+                exampleAddresses.find(({id}) => id === selectedId) : {
+                    address: "",
+                    complete: "",
+                    city: "",
+                },
+        }))
+    }, [selectedId])
 
     return (
         <>
@@ -73,7 +76,7 @@ const Payment = (props) => {
             >
                 <div>
                     <Form>
-                        <div className="flex flex-col items-center my-10 space-y-5">
+                        <div className="flex flex-col items-center my-10">
                             <h1 className="font-bold text-xl">Livraison</h1>
                             <div
                                 className="grid gap-2 grid-cols-1 grid-rows-1 md:grid-cols-2 md:grid-rows-2 lg:grid-cols-4 lg:grid-rows-3 space-y-3 w-4/5 lg:w-1/2">
@@ -81,51 +84,46 @@ const Payment = (props) => {
                                     name="firstName"
                                     label="Prénom"
                                     className="lg:col-span-2"
-                                    value="John"
                                 />
                                 <FormField
                                     name="name"
                                     label="Nom"
                                     className="lg:col-span-2"
-                                    value="Doe"
                                 />
                                 <div className="md:col-span-2 lg:col-span-4 md:w-1/2">
                                     <label htmlFor="addresses"></label>
                                     <select id="addresses"
-                                            onChange={handleInput}
+                                            onChange={getCardId}
                                             className="bg-white border-2 border-gray-400 rounded-lg block w-full px-5 py-2">
+                                        <option defaultChecked>Choisir une addresse enregistrée</option>
                                         {
                                             exampleAddresses
                                                 .map(address =>
-                                                    // eslint-disable-next-line react/jsx-key
-                                                    <option value={address.name} id={address.id}>{address.name}</option>
+                                                    <option value={address.name}
+                                                            id={address.id}
+                                                            key={address.id}
+                                                    >{address.name}</option>
                                                 )
                                         }
                                     </select>
                                 </div>
                                 <FormField
-                                    name="address"
+                                    name="chosen.address"
                                     label="Adresse"
                                     className="lg:col-span-2"
-                                    value={exampleAddresses[selectedId].address}
-
                                 />
                                 <FormField
-                                    name="complete"
+                                    name="chosen.complete"
                                     label="Complément d'adresse"
                                     className="lg:col-span-2"
-                                    value={exampleAddresses[selectedId].complete}
                                 />
                                 <FormField
-                                    name="city"
+                                    name="chosen.city"
                                     label="Ville"
                                     className="lg:col-span-2"
-                                    value={exampleAddresses[selectedId].city}
                                 />
                             </div>
-                            <NavLink href="/payment/payment">
-                                <Button>Payer</Button>
-                            </NavLink>
+                            <Button type="submit">Passer au paiement</Button>
                         </div>
                     </Form>
                 </div>
