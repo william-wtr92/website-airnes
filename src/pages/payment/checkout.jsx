dda44dc870dead24dda9ebf0432e16387ccb2574
Charlotte <charlotte.waegeneire@hotmail.fr>
2023-02-05 18:47:53 +0100

    Fixed typo + flex instead of grid

--- src/pages/payment/checkout.jsx
+++ src/pages/payment/checkout.jsx
@@ -72,26 +72,29 @@ const Checkout = (props) => {
             >
                 <div>
                     <Form>
-                        <div className="flex flex-col items-center my-10">
+                        <div className="flex flex-col items-center my-10 gap-y-5">
                             <h1 className="font-bold text-xl">Livraison</h1>
+
                             <div
-                                className="grid gap-2 grid-cols-1 grid-rows-1 md:grid-cols-2 md:grid-rows-2 lg:grid-cols-4 lg:grid-rows-3 space-y-3 w-4/5 lg:w-1/2">
-                                <FormField
-                                    name="firstName"
-                                    label="Prénom"
-                                    className="lg:col-span-2"
-                                />
-                                <FormField
-                                    name="name"
-                                    label="Nom"
-                                    className="lg:col-span-2"
-                                />
-                                <div className="md:col-span-2 lg:col-span-4 md:w-1/2">
+                                className="flex flex-col flex-wrap md:flex-row w-4/5 lg:w-1/2 gap-y-5">
+                                <div className="basis-full md:basis-1/2">
+                                    <FormField
+                                        name="firstName"
+                                        label="Prénom"
+                                    />
+                                </div>
+                                <div className="basis-full md:basis-1/2">
+                                    <FormField
+                                        name="name"
+                                        label="Nom"
+                                    />
+                                </div>
+                                <div className="basis-full">
                                     <label htmlFor="addresses"></label>
                                     <select id="addresses"
                                             onChange={getCardId}
                                             className="bg-white border-2 border-gray-400 rounded-lg block w-full px-5 py-2">
-                                        <option defaultChecked>Choisir une addresse enregistrée</option>
+                                        <option defaultChecked>Choisir une adresse enregistrée</option>
                                         {
                                             exampleAddresses
                                                 .map(address =>
@@ -106,17 +109,17 @@ const Checkout = (props) => {
                                 <FormField
                                     name="address"
                                     label="Adresse"
-                                    className="lg:col-span-2"
+                                    className="basis-full md:basis-1/2"
                                 />
                                 <FormField
                                     name="complete"
                                     label="Complément d'adresse"
-                                    className="lg:col-span-2"
+                                    className="basis-full md:basis-1/2"
                                 />
                                 <FormField
                                     name="city"
                                     label="Ville"
-                                    className="lg:col-span-2"
+                                    className="w-fit basis-full"
                                 />
                             </div>
                             <Button>Passer au paiement</Button>
