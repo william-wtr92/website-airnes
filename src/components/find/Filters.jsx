576d052acfe13e87cb8b2a13bcbfd181bb8c22eb
Tom OLLIVIER <tom.o@tesapp.io>
2023-02-05 20:56:14 +0100

    SDV-56 setting Search Page

--- src/components/find/Filters.jsx
+++ src/components/find/Filters.jsx
@@ -1,26 +1,29 @@
+
 const Filters = (props) => {
     const {
         categories,
-        onFilterChange,
         name,
+        onFilterChange,
     } = props
 
     return (
         <section
             className="filters"
             aria-labelledby="filters-header">
-            <header id="filters-header" className="pb-3 font-bold">
+            <header id="filters-header" className="pb-6 font-bold text-black text-xl">
                 {name}
             </header>
 
             <ul>
                 {categories.map(category => (
                     <li key={category} >
-                        <label className="text-black font-bold text-md flex gap-2">
+                        <label className="text-black font-bold text-md flex content-center gap-4 text-xl pb-4 pl-4">
                             <input
-                                onChange={onFilterChange}
                                 type="checkbox"
-                                value={category} />
+                                onChange={onFilterChange}
+                                className="border border-black w-[25px] h-[25px]"
+                                value={category}
+                            />
                             {category}
                         </label>
                     </li>
