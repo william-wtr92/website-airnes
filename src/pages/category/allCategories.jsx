52f12b3124d0cd2f2a948116f6b1cd87f2f84ff4
Charlotte <charlotte.waegeneire@hotmail.fr>
2023-02-11 10:47:28 +0100

    all categories update

--- src/pages/category/allCategories.jsx
+++ src/pages/category/allCategories.jsx
@@ -1,9 +1,32 @@
 import Categories from "@/components/home/Categories"
 
 const allCategories = () => {
+    const categories = [
+        "Chambre",
+        "Salle à manger",
+        "Salon",
+        "Salle de bain",
+    ]
+
     return (
         <>
-            <Categories/>
+            <h1
+                className="flex justify-center font-bold tracking-wide text-xl my-10 uppercase"
+            >
+                Toutes nos catégories
+            </h1>
+            <div className="flex flex-wrap justify-center gap-10">
+                {
+                    categories.map(
+                        (category, index) => (
+                            <Categories
+                                key={index}
+                                catName={category}
+                            />
+                        )
+                    )
+                }
+            </div>
         </>
     )
 }
