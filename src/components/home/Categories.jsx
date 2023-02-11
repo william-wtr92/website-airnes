2a13b26431777d3f495d79f8c081a1b6cbe0fb7b
Charlotte <charlotte.waegeneire@hotmail.fr>
2023-02-11 10:31:26 +0100

    Fixed category component

--- src/components/home/Categories.jsx
+++ src/components/home/Categories.jsx
@@ -1,32 +1,18 @@
-const Categories = () => {
-  return (
-    <div className="flex flex-col justify-center items-center mb-10 text-[#EDE4E0] text-4xl font-bold lg:gap-16 lg:flex-row">
-      <div className="lg:hover:scale-105">
-        <p className="flex justify-center relative top-40">CATEGORIE</p>
-        <img
-          src="/images/categories.png"
-          alt="cat1"
-          className="object-cover h-64 w-80"
-        />
-      </div>
-      <div className="lg:hover:scale-105">
-        <p className="flex justify-center relative top-40">CATEGORIE</p>
-        <img
-          src="/images/categories.png"
-          alt="cat2"
-          className="object-cover h-64 w-80"
-        />
-      </div>
-      <div className="lg:hover:scale-105">
-        <p className="flex justify-center relative top-40">CATEGORIE</p>
-        <img
-          src="/images/categories.png"
-          alt="cat3"
-          className="object-cover h-64 w-80"
-        />
-      </div>
-    </div>
-  )
+const Categories = (props) => {
+    const {catName} = props
+
+    return (
+        <div className="h-64 w-80">
+            <p className="flex justify-center relative top-1/2 font-bold tracking-widest text-lg">
+                {catName}
+            </p>
+            <img
+                src="/images/categories.png"
+                alt="cat1"
+                className="object-cover"
+            />
+        </div>
+    )
 }
 
 export default Categories
