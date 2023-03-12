1769ca61c07c6f5d13e345a4a995c8661cf7d192
vaillanh <hugo.vaillant@supdevinci-edu.fr>
2023-03-12 11:51:23 +0100

    Correction

--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,6 @@ config()
 const knexfile = {
   client: "pg",
   connection: {
-    host: process.env.DB_CONNECTION_HOST,
     port: process.env.DB_CONNECTION_PORT,
     user: process.env.DB_CONNECTION_USER,
     password: process.env.DB_CONNECTION_PWD,
