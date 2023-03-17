cae6f39ee9bd12d358a5276da34bd05cd6e2a227
vaillanh <hugo.vaillant@supdevinci-edu.fr>
2023-03-17 16:37:27 +0100

    Setup user settings

---
+++ src/pages/api/userInfo.js
@@ -0,0 +1,37 @@
+import UserModel from "@/api/db/models/UserModel"
+import validate from "@/api/middlewares/validate"
+import mw from "@/api/mw"
+import { numberValidator } from "@/components/validation/validation"
+
+const handler = mw({
+  GET: [
+    validate({
+      query: {
+        userId: numberValidator.required(),
+      },
+    }),
+    async ({
+      locals: {
+        query: { userId },
+      },
+      res,
+    }) => {
+      const id = userId
+
+      const query = await UserModel.query()
+        .findOne({ id })
+        .select("email", "name")
+        .withGraphFetched("alldata")
+
+      if (query) {
+        res.send({
+          result: query,
+        })
+      } else {
+        res.send({ result: { email: "", name: "", alldata: [] } })
+      }
+    },
+  ],
+})
+
+export default handler
--- src/web/routes.js
+++ src/web/routes.js
@@ -1,3 +1,13 @@
+const createRouteWithQueryParams = (route, query) => {
+  if (!query) {
+    return route
+  }
+
+  const qs = new URLSearchParams(query).toString()
+
+  return `${route}?${qs}`
+}
+
 const routes = {
   home: () => "/",
   signUp: () => "/sign-up",
@@ -8,6 +18,10 @@ const routes = {
     signIn: () => "/sign-in",
     contact: () => "/contact",
     addAdress: () => "/create/address",
+    userData: () => "/userInfo",
+    userInfo: {
+      userData: (query) => createRouteWithQueryParams("/userInfo", query),
+    },
   },
 }
 
