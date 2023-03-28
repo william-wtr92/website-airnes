2a776f58469dbabdc9bd1243c6551870ec0f3c6d
Charlotte <charlotte.waegeneire@hotmail.fr>
2023-03-28 11:24:01 +0200

    edited date format

--- src/pages/admin/contacts/[contactId]/show.jsx
+++ src/pages/admin/contacts/[contactId]/show.jsx
@@ -29,10 +29,23 @@ export const getServerSideProps = async (context) => {
 const ShowContact = (props) => {
   const {contact} = props
 
+  const formatDate = (dateString) => {
+    const date = new Date(dateString)
+    const year = date.getFullYear()
+    const month = String(date.getMonth() + 1).padStart(2, "0")
+    const day = String(date.getDate()).padStart(2, "0")
+
+    return `${day}/${month}/${year}`
+  }
+
   return (
     <div className="p-10 flex flex-col gap-10 absolute top-10 left-0 z-0 lg:top-0 lg:left-64">
       <Return name="categories" back={"/admin/contacts/all"}/>
       <div>{contact.mail}</div>
+      <div>
+        {formatDate(contact.createdAt)}
+        {contact.updatedAt !== contact.createdAt ? `(Last udpdate: ${formatDate(contact.updatedAt)}` : null}
+      </div>
       <div className="font-bold">
         {contact.topic}
       </div>
@@ -42,7 +55,7 @@ const ShowContact = (props) => {
       <NavLink href="#">
         <button className="uppercase bg-white text-gray-500 font-bold rounded-full border-2 px-4 py-1">
           {
-            contact.status ? "Re-open it" :"Mark as read"
+            contact.status ? "Re-open it" : "Mark as read"
           }
         </button>
       </NavLink>
