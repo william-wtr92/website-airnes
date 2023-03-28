823f9ae5019fbf767186be53b70f0848482ec59b
Tom OLLIVIER <tom.o@tesapp.io>
2023-03-28 13:36:38 +0200

    connected user can go to info page and have their name display

--- src/components/layouts/navs/Users.jsx
+++ src/components/layouts/navs/Users.jsx
@@ -7,11 +7,25 @@ import {
   UserIcon,
   XMarkIcon,
 } from "@heroicons/react/24/solid"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { Collapse } from "@/components/app/ui/Collapse"
+import config from "@/web/config"
+import parseSession from "@/web/parseSession"
 
 const Users = () => {
   const [burgerMenu, setBurgerMenu] = useState(false)
+  const [token, setToken ] = useState(false)
+
+
+  useEffect(() => {
+    const jwt = localStorage.getItem(config.session.localStorageKey)
+
+    if (jwt) {
+      setToken(parseSession(jwt))
+    }
+  }, [])
+
+
 
   const categoryOptions = [
     {
@@ -58,88 +72,88 @@ const Users = () => {
   cartNumber = cartNumber === 0 ? null : cartNumber > 9 ? "9+" : cartNumber
 
   return (
-    <>
-      <header className="bg-white sticky top-0 z-20">
-        <div className="flex h-14 items-center shadow-sm shadow-[#615043]">
-          <div className="flex mr-auto ml-2 mt-2 lg:ml-10">
-            <NavLink href="/">
-              <Image
-                src="/images/logo.png"
-                alt="Logo"
-                width={100}
-                height={1}
-                className="h-24 w-24 hover:scale-110 lg:w-24"
-              />
-            </NavLink>
-          </div>
-          <div className="flex gap-2 lg:gap-6">
-            <NavLink href="/user/login">
-              <UserIcon
-                className={`h-6 hover:scale-110 hover:text-[#b3825c]`}
-                color={"#615043"}
-              />
-            </NavLink>
-            <NavLink href="/categories/search">
-              <MagnifyingGlassIcon
-                className={`h-6 hover:scale-110 hover:text-[#b3825c]`}
-                color={"#615043"}
-              />
-            </NavLink>
-            <NavLink href="/user/userId/cart">
-              <ShoppingCartIcon
-                className={`h-6 hover:scale-110 hover:text-[#b3825c]`}
-                color={"#615043"}
-              />
+      <>
+        <header className="bg-white sticky top-0 z-20">
+          <div className="flex h-14 items-center shadow-sm shadow-[#615043]">
+            <div className="flex mr-auto ml-2 mt-2 lg:ml-10">
+              <NavLink href="/">
+                <Image
+                    src="/images/logo.png"
+                    alt="Logo"
+                    width={100}
+                    height={1}
+                    className="h-24 w-24 hover:scale-110 lg:w-24"
+                />
+              </NavLink>
+            </div>
+            <div className="flex gap-2 lg:gap-6">
+              <NavLink href={token ? `/user/${token.user.id}/home`: `/user/login`}>
+                <UserIcon
+                    className={`h-6 hover:scale-110 hover:text-[#b3825c]`}
+                    color={"#615043"}
+                />
+              </NavLink>
+              <NavLink href="/categories/search">
+                <MagnifyingGlassIcon
+                    className={`h-6 hover:scale-110 hover:text-[#b3825c]`}
+                    color={"#615043"}
+                />
+              </NavLink>
+              <NavLink href="/user/userId/cart">
+                <ShoppingCartIcon
+                    className={`h-6 hover:scale-110 hover:text-[#b3825c]`}
+                    color={"#615043"}
+                />
 
-              {cartNumber !== null && (
-                <div
-                  className={`absolute top-7 px-2 ${
-                    cartNumber < 9
-                      ? "right-12 lg:right-16"
-                      : "right-[43px] lg:right-[58px]"
-                  } -z-10 bg-[#EDE4E0] text-primary font-bold text-xs rounded-full lg:px-2`}
-                >
-                  {cartNumber}
-                </div>
-              )}
-            </NavLink>
-          </div>
-          <div className="ml-6 mr-4 lg:ml-10">
-            <Bars3Icon
-              onClick={toggleBurgerMenu}
-              className={`h-6 hover:scale-110 hover:cursor-pointer hover:text-[#b3825c]`}
-              color={"#615043"}
-            />
-          </div>
-          <div
-            className={`${
-              burgerMenu ? `block ` : `hidden `
-            }w-full md:w-[325px] h-screen bg-[#ffffff] absolute inset-y-0 right-0 z-50 opacity-95`}
-          >
-            <div>
-              <XMarkIcon
-                className="h-6 hover:cursor-pointer relative top-4 left-3 hover:scale-105"
-                onClick={toggleBurgerMenu}
+                {cartNumber !== null && (
+                    <div
+                        className={`absolute top-7 px-2 ${
+                            cartNumber < 9
+                                ? "right-12 lg:right-16"
+                                : "right-[43px] lg:right-[58px]"
+                        } -z-10 bg-[#EDE4E0] text-primary font-bold text-xs rounded-full lg:px-2`}
+                    >
+                      {cartNumber}
+                    </div>
+                )}
+              </NavLink>
+            </div>
+            <div className="ml-6 mr-4 lg:ml-10">
+              <Bars3Icon
+                  onClick={toggleBurgerMenu}
+                  className={`h-6 hover:scale-110 hover:cursor-pointer hover:text-[#b3825c]`}
+                  color={"#615043"}
               />
             </div>
-            <div className="flex flex-col mx-16 my-10 gap-10">
-              <div className="hover:text-[#615043] hover:scale-105">
-                <NavLink href="/user/login">Connexion</NavLink>
+            <div
+                className={`${
+                    burgerMenu ? `block ` : `hidden `
+                }w-full md:w-[325px] h-screen bg-[#ffffff] absolute inset-y-0 right-0 z-50 opacity-95`}
+            >
+              <div>
+                <XMarkIcon
+                    className="h-6 hover:cursor-pointer relative top-4 left-3 hover:scale-105"
+                    onClick={toggleBurgerMenu}
+                />
               </div>
-              <div className="hover:text-[#615043] hover:scale-105">
-                <Collapse title="Catégories" content={categoryOptions} />
-              </div>
-              <div className="hover:text-[#615043] hover:scale-105">
-                <Collapse title="Produits" content={productOptions} />
-              </div>
-              <div className="hover:text-[#615043] hover:scale-105">
-                <NavLink href="/">Promotions</NavLink>
+              <div className="flex flex-col mx-16 my-10 gap-10">
+                <div className="hover:text-[#615043] hover:scale-105">
+                  <NavLink href="/user/login">Connexion</NavLink>
+                </div>
+                <div className="hover:text-[#615043] hover:scale-105">
+                  <Collapse title="Catégories" content={categoryOptions} />
+                </div>
+                <div className="hover:text-[#615043] hover:scale-105">
+                  <Collapse title="Produits" content={productOptions} />
+                </div>
+                <div className="hover:text-[#615043] hover:scale-105">
+                  <NavLink href="/">Promotions</NavLink>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </header>
-    </>
+        </header>
+      </>
   )
 }
 
--- src/pages/user/[userId]/home.jsx
+++ src/pages/user/[userId]/home.jsx
@@ -1,25 +1,59 @@
 import FooterMenu from "@/components/layouts/FooterMenu"
 import Button from "@/components/app/ui/Button"
 import { NavLink } from "@/components/utils/NavLink"
+import {useRouter} from "next/router"
+import {useEffect} from "react"
+import config from "@/web/config"
+import parseSession from "@/web/parseSession"
+import axios from "axios"
+import routes from "@/web/routes"
 
-const UserHome = () => {
+export const getServerSideProps = async (context) => {
+  const { query } = context
+
+  const { data } = await axios.get(
+      `http://localhost:3000/api${routes.api.userInfo.usesrData(query)}`
+  )
+
+  return {
+    props: {
+      data: data,
+      userId: query.userId,
+    },
+  }
+}
+const UserHome = (props) => {
+  const { data, userId } = props
+
+  const router = useRouter()
+  useEffect(() => {
+    const jwt = localStorage.getItem(config.session.localStorageKey)
+
+    const session = parseSession(jwt)
+
+    if (session.user.id != userId) {
+      router.push("/")
+    }
+  }, [userId, router])
+
+
   return (
-    <>
-      <div className="gap-16 flex flex-col justify-center mx-6 mt-20 py-10 px-10 lg:w-[450px] lg:py-16 lg:mx-auto  lg:mt-28">
-        <div className="text-center text-3xl text-black">
-          Bonjour (Surname and Name)
+      <>
+        <div className="gap-16 flex flex-col justify-center mx-6 mt-20 py-10 px-10 lg:w-[450px] lg:py-16 lg:mx-auto  lg:mt-28">
+          <div className="text-center text-3xl text-black">
+            Bonjour {data.result.name}
+          </div>
+          <NavLink href="/user/userId/orders">
+            <Button className="w-full rounded-2xl">MES COMMANDES</Button>
+          </NavLink>
+          <NavLink href="/user/userId/settings">
+            <Button className="w-full rounded-2xl">
+              MES INFORMATIONS PERSONELLES
+            </Button>
+          </NavLink>
         </div>
-        <NavLink href="/user/userId/orders">
-          <Button className="w-full rounded-2xl">MES COMMANDES</Button>
-        </NavLink>
-        <NavLink href="/">
-          <Button className="w-full rounded-2xl">
-            MES INFORMATIONS PERSONELLES
-          </Button>
-        </NavLink>
-      </div>
-      <FooterMenu position="absolute" />
-    </>
+        <FooterMenu position="absolute" />
+      </>
   )
 }
 
