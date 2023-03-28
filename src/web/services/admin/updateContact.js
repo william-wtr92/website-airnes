983988f5567961457de8e8ac07e6c3373e57840e
Charlotte <charlotte.waegeneire@hotmail.fr>
2023-03-28 22:03:53 +0200

    Mark a contact request as read/new + fixed categories routes

---
+++ src/web/services/admin/updateContact.js
@@ -0,0 +1,21 @@
+import routes from "@/web/routes"
+
+const updateContact =
+  ({api, jwt}) =>
+    async ({contactId, status}) => {
+      try {
+        const {data} = await api.patch(routes.api.updateContact(contactId), {
+          status,
+          jwt,
+        })
+
+        return [null, data]
+      } catch (err) {
+        const error = err.response?.data?.error || "Oops. Something went wrong"
+
+        return [Array.isArray(error) ? error : [error]]
+      }
+    }
+
+
+export default updateContact
