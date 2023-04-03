a22d087224f342a82f0a5234c6ddecae8b3786bd
William <wwautrin@hotmail.fr>
2023-04-03 15:52:03 +0200

    Product front rendering

--- src/components/app/content/Product.jsx
+++ src/components/app/content/Product.jsx
@@ -1,29 +1,62 @@
+import axios from "axios"
 import Image from "next/image"
+import { useEffect, useState } from "react"
+import routes from "@/web/routes"
 
-const Product = (props) => {
-  const { image, alt, productName, productPrice, promotion } = props
+const Product = () => {
+  const [products, setProducts] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get(
+        `http://localhost:3000/api${routes.api.getProducts()}`
+      )
+
+      setIsLoading(false)
+      setProducts(data.result)
+    }
+
+    fetchProducts()
+  }, [])
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
+  if (products.length === 0) {
+    return <div>No products available</div>
+  }
 
   return (
     <>
       <div>
-        <Image
-          src={image}
-          alt={alt}
-          className="border-2 border-black w-full h-60 object-cover"
-          width={500}
-          height={500}
-        />
-        <div className="flex justify-between font-extrabold uppercase p-1">
-          <h1>{productName}</h1>
-          {promotion ? (
-            <div className="text-right">
-              <p className="line-through">{productPrice} €</p>
-              <p className="text-red-600 text-lg">{promotion} €</p>
-            </div>
-          ) : (
-            <p>{productPrice} €</p>
-          )}
-        </div>
+        {products.map((product) => {
+          return (
+            <>
+              <Image
+                src={product.image}
+                alt={product.name}
+                className="border-2 border-black w-full h-60 object-cover"
+                width={500}
+                height={500}
+              />
+              <div className="flex justify-between font-extrabold uppercase p-1">
+                <h1>{product.name}</h1>
+                {product.promotion ? (
+                  <div className="text-right">
+                    <p className="line-through">{product.price} €</p>
+                    <p className="text-red-600 text-lg">
+                      {product.promotion} €
+                    </p>
+                  </div>
+                ) : (
+                  <p>{product.price} €</p>
+                )}
+              </div>
+            </>
+          )
+        })}
       </div>
     </>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,58 +4,6 @@ import FooterMenu from "@/components/layouts/FooterMenu"
 import Product from "@/components/app/content/Product"
 
 const Main = () => {
-  const exampleState = [
-    {
-      id: 0,
-      name: "Chaise",
-      price: 123,
-      promotion: 100,
-      image: "/images/meuble2.jpg",
-    },
-    {
-      id: 1,
-      name: "Chaise 2",
-      price: 456,
-      promotion: 400,
-      image: "/images/meuble2.jpg",
-    },
-    {
-      id: 2,
-      name: "Chaise 3",
-      price: 789,
-      promotion: 700,
-      image: "/images/meuble2.jpg",
-    },
-    {
-      id: 3,
-      name: "Chaise 4",
-      price: 987,
-      promotion: 900,
-      image: "/images/meuble2.jpg",
-    },
-    {
-      id: 4,
-      name: "Chaise 5",
-      price: 654,
-      promotion: 600,
-      image: "/images/meuble2.jpg",
-    },
-    {
-      id: 5,
-      name: "Chaise 5",
-      price: 654,
-      promotion: 600,
-      image: "/images/meuble2.jpg",
-    },
-    {
-      id: 6,
-      name: "Chaise 5",
-      price: 654,
-      promotion: 600,
-      image: "/images/meuble2.jpg",
-    },
-  ]
-
   return (
     <>
       <main>
@@ -78,17 +26,7 @@ const Main = () => {
               Promotions
             </h3>
             <div className="overflow-x-auto scrollbar flex w-full gap-10 bg-gray-100 p-8">
-              {exampleState.map((product) => (
-                <div key={product.id} className="flex-none w-3/5 lg:w-2/5">
-                  <Product
-                    alt="test"
-                    image={product.image}
-                    productName={product.name}
-                    productPrice={product.price}
-                    promotion={product.promotion}
-                  />
-                </div>
-              ))}
+              <Product />
             </div>
           </div>
         </div>
