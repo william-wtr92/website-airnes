9c3228ba802a0a2b357d91a3fd5247bc7d4db29f
Tom OLLIVIER <tom.o@tesapp.io>
2023-04-03 23:33:44 +0200

    SDV-292 as an admin i can see a specific product and the table of product

---
+++ src/pages/admin/products/all.jsx
@@ -0,0 +1,52 @@
+import axios from "axios"
+import routes from "@/web/routes"
+import DisplayPage from "@/components/app/admin/DisplayPage"
+
+export const getServerSideProps = async (context) => {
+  const { page } = context.query
+
+  const redirectToInitial = () => {
+    return {
+      redirect: {
+        destination: "/admin/products/all",
+        permanent: false,
+      },
+    }
+  }
+
+  try {
+    const { data } = await axios.get(
+      `http://localhost:3000/api${routes.api.getProducts()}?page=${page || 1}`
+    )
+
+    const isEmpty = data.result.length === 0
+
+    if (isEmpty) {
+      return redirectToInitial()
+    }
+
+    return {
+      props: {
+        products: data.result,
+        pagination: data.pagination,
+      },
+    }
+  } catch (error) {
+    return redirectToInitial()
+  }
+}
+
+const All = (props) => {
+  const { products, pagination } = props
+
+  return (
+    <DisplayPage
+      sections={"products"}
+      section={"products"}
+      items={products}
+      pagination={pagination}
+    />
+  )
+}
+
+export default All
--- src/pages/api/admin/products/product.js
+++ src/pages/api/admin/products/product.js
@@ -2,10 +2,12 @@ import ProductModel from "@/api/db/models/ProductModel"
 import validate from "@/api/middlewares/validate"
 import mw from "@/api/mw"
 import {
-    linkValidator, numberValidator, stringValidator,
+    linkValidator, numberValidator, queryPageValidator, stringValidator,
 } from "@/components/validation/validation"
 import parseSession from "@/web/parseSession"
 import UserModel from "@/api/db/models/UserModel"
+import config from "@/api/config"
+import {NotFoundError} from "@/api/errors"
 
 const handler = mw({
     POST: [
@@ -53,6 +55,45 @@ const handler = mw({
                 materialId,
             })
             res.send({result: true})
+        },
+    ],
+
+    GET: [
+        validate({
+            query: {
+                page: queryPageValidator,
+            },
+        }),
+        async ({
+                   locals: {
+                       query: { page },
+                   },
+                   res,
+               }) => {
+            const limit = config.pagination.limit.default
+            const offset = (page - 1) * limit
+
+            const products = await ProductModel.query()
+                .orderBy("id", "asc")
+                .limit(limit)
+                .offset(offset)
+            const totalCount = await ProductModel.query().count().first()
+
+            if (products) {
+                res.send({
+                    result: products,
+                    pagination: {
+                        page,
+                        limit,
+                        totalItems: parseInt(totalCount.count, 10),
+                        totalPages: Math.ceil(totalCount.count / limit),
+                    },
+                })
+            } else {
+                res.send({ result: "" })
+
+                throw new NotFoundError()
+            }
         },
     ],
 })
