ed3f6533799593956907e78120996914e4a988be
William <wwautrin@hotmail.fr>
2023-04-12 22:53:57 +0200

    Update NavBar

--- src/components/app/ui/Collapse.jsx
+++ src/components/app/ui/Collapse.jsx
@@ -1,32 +1,33 @@
-import {useState} from "react"
-import {NavLink} from "@/components/utils/NavLink"
+import { useState } from "react"
+import { NavLink } from "@/components/utils/NavLink"
 
 export const Collapse = (props) => {
-    const [isCollapsed, setCollapse] = useState(false)
-    const {title, content} = props
+  const [isCollapsed, setCollapse] = useState(false)
+  const { title, content } = props
 
-    const handleCollapse = () => {
-        setCollapse(!isCollapsed)
-    }
+  const handleCollapse = () => {
+    setCollapse(!isCollapsed)
+  }
 
-    return (
-        <div>
-            <div onClick={handleCollapse}>{title}</div>
-            <div className={isCollapsed ? "block" : "hidden"}>
-                <ul>
-                    {
-                        content.map(
-                            (list, index) => (
-                                <li className="indent-4" key={index}>
-                                    <NavLink href={list.redirection}>
-                                        {list.name}
-                                    </NavLink>
-                                </li>
-                            )
-                        )
-                    }
-                </ul>
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <div
+        onClick={handleCollapse}
+        className="hover:scale-105 hover:cursor-pointer hover:text-[#6f5e3f]"
+      >
+        {title}
+      </div>
+      <div className={isCollapsed ? "block" : "hidden"}>
+        <ul>
+          {content.map((list) => (
+            <li className="indent-4" key={list.id}>
+              <NavLink href={`/categories/${list.id}/category`}>
+                <p className="hover:text-[#6f5e3f]">{list.name}</p>
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
 }
