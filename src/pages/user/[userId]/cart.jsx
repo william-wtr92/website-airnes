6afa3837ce436afc44fa551327f192b42806d0fe
William <wwautrin@hotmail.fr>
2023-04-13 21:31:51 +0200

    patch responsive

--- src/pages/user/[userId]/cart.jsx
+++ src/pages/user/[userId]/cart.jsx
@@ -96,70 +96,79 @@ const UserCart = () => {
         </div>
 
         <div className="flex flex-col md:flex-col lg:flex-col xl:flex-row">
-          <div className="flex flex-col md:justify-center lg:justify-center xl:justify-start">
-            {cartItems.map((item) => (
-              <div key={item.id} className="mb-4">
-                <div className="flex shadow-xl bg-[#fff] mx-auto w-[300px] justify-start px-4 py-2 lg:w-[550px] lg:mx-36">
-                  <div className="flex mt-2 lg:items-center lg:mt-0">
-                    <Image
-                      src={item.image}
-                      alt={item.name}
-                      width={100}
-                      height={1}
-                      className="w-20 object-cover h-20 lg:h-24 lg:w-28 hover:cursor-pointer"
-                    />
-                  </div>
+          {cartItems.length === 0 ? (
+            <div className="flex justify-center lg:mt-10 xl:w-1/2">
+              <h2 className="font-bold text-xl">Panier vide</h2>
+            </div>
+          ) : (
+            <div className="flex flex-col justify-center xl:justify-start">
+              {cartItems.map((item) => (
+                <div key={item.id} className="flex justify-center mb-4">
+                  <div className="flex shadow-xl bg-[#fff] mx-auto w-[300px] justify-start px-4 py-2 lg:w-[550px] lg:mx-36">
+                    <div className="flex mt-2 lg:items-center lg:mt-0">
+                      <Image
+                        src={item.image}
+                        alt={item.name}
+                        width={100}
+                        height={1}
+                        className="w-20 object-cover h-20 lg:h-24 lg:w-28 hover:cursor-pointer"
+                      />
+                    </div>
 
-                  <div className="w-32 lg:w-1/2 lg:ml-10">
-                    <NavLink href={`/products/${item.id}/product`}>
-                      <p className="text-sm mt-2 ml-4 font-bold hover:text-[#b3825c] lg:mt-0 lg:ml-0 lg:text-md">
-                        {item.name}
+                    <div className="w-32 lg:w-1/2 lg:ml-10">
+                      <NavLink href={`/products/${item.id}/product`}>
+                        <p className="text-sm mt-2 ml-4 font-bold hover:text-[#b3825c] lg:mt-0 lg:ml-0 lg:text-md">
+                          {item.name}
+                        </p>
+                      </NavLink>
+                      <p className="hidden text-sm mt-2 lg:block">
+                        {item.description}
                       </p>
-                    </NavLink>
-                    <p className="hidden text-sm mt-2 lg:block">
-                      {item.description}
-                    </p>
-                  </div>
-                  <div className="flex flex-col relative left-5 lg:left-12">
-                    <p className="font-bold mb-2 lg:mb-2">{item.price} €</p>
-                    <input
-                      type="number"
-                      min="1"
-                      max={parseInt(item.quantity, 10)}
-                      className="w-12 h-6 border-2 rounded-md pl-2 mb-2 lg:mb-4"
-                      value={item.product_quantity}
-                      onChange={(e) =>
-                        handleQuantityChange(item.id, parseInt(e.target.value))
-                      }
-                    />
+                    </div>
+                    <div className="flex flex-col relative left-5 lg:left-12">
+                      <p className="font-bold mb-2 lg:mb-2">{item.price} €</p>
+                      <input
+                        type="number"
+                        min="1"
+                        max={parseInt(item.quantity, 10)}
+                        className="w-12 h-6 border-2 rounded-md pl-2 mb-2 lg:mb-4"
+                        value={item.product_quantity}
+                        onChange={(e) =>
+                          handleQuantityChange(
+                            item.id,
+                            parseInt(e.target.value)
+                          )
+                        }
+                      />
 
-                    <div className="flex flex-col gap-2">
-                      {isMobile ? (
+                      <div className="flex flex-col gap-2">
+                        {isMobile ? (
+                          <div className="w-10 relative left-4">
+                            <PlusIcon
+                              className="h-4"
+                              onClick={() => handleIncrement(item.id)}
+                            />
+                            <MinusIcon
+                              className="h-4"
+                              onClick={() => handleDecrement(item.id)}
+                            />
+                          </div>
+                        ) : (
+                          ""
+                        )}
                         <div className="w-10 relative left-4">
-                          <PlusIcon
-                            className="h-4"
-                            onClick={() => handleIncrement(item.id)}
-                          />
-                          <MinusIcon
-                            className="h-4"
-                            onClick={() => handleDecrement(item.id)}
+                          <TrashIcon
+                            className="h-6 hover:cursor-pointer hover:text-[#927864]"
+                            onClick={() => handleRemoveFromCart(item.id)}
                           />
                         </div>
-                      ) : (
-                        ""
-                      )}
-                      <div className="w-10 relative left-4">
-                        <TrashIcon
-                          className="h-6 hover:cursor-pointer hover:text-[#927864]"
-                          onClick={() => handleRemoveFromCart(item.id)}
-                        />
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            ))}
-          </div>
+              ))}
+            </div>
+          )}
 
           <div>
             <div className="flex flex-col items-center mt-10">
@@ -174,7 +183,7 @@ const UserCart = () => {
               <div className="my-6">
                 <Button
                   className="bg-[#615043] hover:bg-[#927864] hover:cursor-pointer
-             active:bg-[#615043] border border-black px-10 py-4 font-semibold rounded-md text-[#fff]"
+           active:bg-[#615043] border border-black px-10 py-4 font-semibold rounded-md text-[#fff]"
                 >
                   <NavLink href="/payment/checkout">Passer la Commande</NavLink>
                 </Button>
