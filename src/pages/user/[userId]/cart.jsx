b7ceccf3fa42fb4e392abeb1691abfb2766f4b07
William <wwautrin@hotmail.fr>
2023-04-13 17:09:47 +0200

    Add protection with sotck value & add quantity on tablet and phone

--- src/pages/user/[userId]/cart.jsx
+++ src/pages/user/[userId]/cart.jsx
@@ -1,10 +1,11 @@
 import Button from "@/components/app/ui/Button"
-import { TrashIcon } from "@heroicons/react/24/solid"
+import { TrashIcon, PlusIcon, MinusIcon } from "@heroicons/react/24/solid"
 import { NavLink } from "@/components/utils/NavLink"
 import Image from "next/image"
 import axios from "axios"
 import routes from "@/web/routes"
 import { useCart } from "@/web/hooks/cart"
+import { useMediaQuery } from "react-responsive"
 
 export const getServerSideProps = async (context) => {
   const { query } = context
@@ -34,12 +35,41 @@ export const getServerSideProps = async (context) => {
 
 const UserCart = () => {
   const { cartItems, total, tax, removeFromCart, updateQuantity } = useCart()
+  const isMobile = useMediaQuery({ query: "(max-width: 1000px)" })
 
   const handleRemoveFromCart = (productId) => {
     removeFromCart(productId)
   }
 
   const handleQuantityChange = (productId, newQuantity) => {
+    const item = cartItems.find((item) => item.id === productId)
+
+    if (newQuantity > parseInt(item.quantity, 10)) {
+      newQuantity = parseInt(item.quantity, 10)
+    }
+
+    updateQuantity(productId, newQuantity)
+  }
+
+  const handleIncrement = (productId) => {
+    const item = cartItems.find((item) => item.id === productId)
+    const newQuantity = parseInt(item.product_quantity, 10) + 1
+
+    if (newQuantity > parseInt(item.quantity, 10)) {
+      return
+    }
+
+    updateQuantity(productId, newQuantity)
+  }
+
+  const handleDecrement = (productId) => {
+    const item = cartItems.find((item) => item.id === productId)
+    const newQuantity = parseInt(item.product_quantity, 10) - 1
+
+    if (newQuantity < 1) {
+      return
+    }
+
     updateQuantity(productId, newQuantity)
   }
 
@@ -80,17 +110,35 @@ const UserCart = () => {
                 <input
                   type="number"
                   min="1"
+                  max={parseInt(item.quantity, 10)}
                   className="w-12 h-6 border-2 rounded-md pl-2 mb-2 lg:mb-4"
                   value={item.product_quantity}
                   onChange={(e) =>
                     handleQuantityChange(item.id, parseInt(e.target.value))
                   }
                 />
-                <div className="w-10 relative left-4">
-                  <TrashIcon
-                    className="h-6 hover:cursor-pointer hover:text-[#927864]"
-                    onClick={() => handleRemoveFromCart(item.id)}
-                  />
+
+                <div className="flex flex-col gap-2">
+                  {isMobile ? (
+                    <div className="w-10 relative left-4">
+                      <PlusIcon
+                        className="h-4"
+                        onClick={() => handleIncrement(item.id)}
+                      />
+                      <MinusIcon
+                        className="h-4"
+                        onClick={() => handleDecrement(item.id)}
+                      />
+                    </div>
+                  ) : (
+                    ""
+                  )}
+                  <div className="w-10 relative left-4">
+                    <TrashIcon
+                      className="h-6 hover:cursor-pointer hover:text-[#927864]"
+                      onClick={() => handleRemoveFromCart(item.id)}
+                    />
+                  </div>
                 </div>
               </div>
             </div>
