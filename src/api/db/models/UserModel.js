4e263068fee19e3e598e9c8c28074e5bd6587832
vaillanh <hugo.vaillant@supdevinci-edu.fr>
2023-04-25 12:08:42 +0200

    correction import

--- src/api/db/models/UserModel.js
+++ src/api/db/models/UserModel.js
@@ -1,7 +1,7 @@
 import BaseModel from "@/api/db/models/BaseModel.js"
 import AddressModel from "./AddressModel"
 import RoleModel from "./RoleModel"
-const { hashPassword } = require("@/api/db/hashPassword")
+import hashPassword from "../hashPassword"
 
 class UserModel extends BaseModel {
   static tableName = "user"
