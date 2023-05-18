38562ae6598d75058ebd02cc9fcc31de81fcb4e9
William <wwautrin@hotmail.fr>
2023-05-18 11:16:10 +0200

    Trad of reset pwd & forgot pwd

--- src/pages/user/reset_password.jsx
+++ src/pages/user/reset_password.jsx
@@ -8,12 +8,22 @@ import {
   resetpwdInitialValues,
   resetpwdValidationSchema,
 } from "@/components/validation/reset_password"
+import { useTranslation } from "next-i18next"
+import { serverSideTranslations } from "next-i18next/serverSideTranslations"
 
 export const getServerSideProps = async (context) => {
   const { tokken } = context.query
+  const { locale } = context
+
+  const translations = await serverSideTranslations(locale, [
+    "resetpwd",
+    "navbar",
+    "footer",
+  ])
 
   return {
     props: {
+      ...translations,
       tokken,
     },
   }
@@ -44,6 +54,8 @@ const OnForgotPassword = (props) => {
     [resetPwd, router, tokken]
   )
 
+  const { t } = useTranslation("resetpwd")
+
   return (
     <>
       <Formik
@@ -53,26 +65,24 @@ const OnForgotPassword = (props) => {
         error={error}
       >
         <div className="gap-10 flex flex-col justify-center mx-6 mt-20 py-10 px-10 lg:w-[450px] lg:py-16 lg:mx-auto  lg:mt-28">
-          <div className="text-center text-3xl text-black">
-            Mot de passe oublié
-          </div>
+          <div className="text-center text-3xl text-black">{t(`lostText`)}</div>
           <Form className="flex flex-col">
             <Formfield
               type="password"
               name="password"
-              placeholder="Entrez votre nouveau mot de passe"
-              label="Mot de passe"
+              placeholder={t(`enterPlaceholder`)}
+              label={t(`enterLabel`)}
               className="mb-2 pb-10"
             />
             <Formfield
               type="password"
               name="passwordConfirmation"
-              placeholder="Comfirmé votre mot de passe"
-              label="Comfirmation mot de passe"
+              placeholder={t(`confirmPlaceholder`)}
+              label={t(`confirmLabel`)}
               className="mb-2 pb-10"
             />
             <div className="mt-6">
-              <Button className="w-full rounded-2xl">Comfirmé</Button>
+              <Button className="w-full rounded-2xl">{t(`buttonText`)}</Button>
             </div>
           </Form>
         </div>
