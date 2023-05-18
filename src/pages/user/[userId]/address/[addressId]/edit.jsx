698ff2f9becd7f267da7e0f48aa38e88a933a40f
William <wwautrin@hotmail.fr>
2023-05-18 12:43:53 +0200

    Trad of add & edit address

--- src/pages/user/[userId]/address/[addressId]/edit.jsx
+++ src/pages/user/[userId]/address/[addressId]/edit.jsx
@@ -8,9 +8,11 @@ import axios from "axios"
 import routes from "@/web/routes"
 import useAppContext from "@/web/hooks/useAppContext"
 import config from "@/api/config"
+import { useTranslation } from "next-i18next"
+import { serverSideTranslations } from "next-i18next/serverSideTranslations"
 
 export const getServerSideProps = async (context) => {
-  const { query } = context
+  const { query, locale } = context
 
   const { data } = await axios.get(
     `${config.path}api${routes.api.user.address.addressData(
@@ -18,6 +20,12 @@ export const getServerSideProps = async (context) => {
       query.addressId
     )}`
   )
+
+  const translations = await serverSideTranslations(locale, [
+    "editaddress",
+    "navbar",
+    "footer",
+  ])
 
   if (!data.result) {
     return {
@@ -33,6 +41,7 @@ export const getServerSideProps = async (context) => {
       data: data,
       userId: query.userId,
       addressId: query.addressId,
+      ...translations,
     },
   }
 }
@@ -73,6 +82,8 @@ const EditAddress = (props) => {
     [patchAddress, router, userId, addressId]
   )
 
+  const { t } = useTranslation("editaddress")
+
   return (
     <>
       <Formik
@@ -85,58 +96,58 @@ const EditAddress = (props) => {
           <div className="flex flex-col my-10 items-center ">
             <div className="w-4/5 lg:w-2/5">
               <h1 className="font-bold text-xl my-8 text-center">
-                Modification de l'adresse
+                {t(`addText`)}
               </h1>
               <div className="flex flex-col lg:flex-row mb-4  gap-4 lg:gap-12">
                 <FormField
                   name="name"
-                  placeholder="Michel"
-                  label="Prénom"
+                  placeholder={t(`firstnamePlaceholder`)}
+                  label={t(`firstnameLabel`)}
                   className="lg:w-2/5"
                 />
                 <FormField
                   name="lastName"
-                  placeholder="Michel"
-                  label="Nom"
+                  placeholder={t(`lastnamePlaceholder`)}
+                  label={t(`lastnameLabel`)}
                   className="lg:w-2/5"
                 />
               </div>
               <FormField
                 name="addressName"
-                placeholder="Domicile"
-                label="Nom de l'adresse"
+                placeholder={t(`addressNamePlaceholder`)}
+                label={t(`addressNameLabel`)}
                 className="w-3/5 mb-4"
               />
               <div className="flex flex-col lg:flex-row mb-4 gap-4 lg:gap-12">
                 <FormField
                   name="address"
-                  placeholder="01 rue de ..."
-                  label="Adresse"
+                  placeholder={t(`addressFullPlaceholder`)}
+                  label={t(`addressFullLabel`)}
                   className="lg:w-2/5"
                 />
                 <FormField
                   name="complete"
-                  placeholder="bâtiment, rue..."
-                  label="Complément d'adresse"
+                  placeholder={t(`addressComplementPlaceholder`)}
+                  label={t(`addressComplementLabel`)}
                   className="lg:w-2/5"
                 />
               </div>
               <div className="flex flex-col lg:flex-row mb-4 lg:mb-12 gap-4 lg:gap-12">
                 <FormField
                   name="postal_code"
-                  placeholder="00000"
-                  label="Code Postal"
+                  placeholder={t(`postalPlaceholder`)}
+                  label={t(`postalLabel`)}
                   className="lg:w-2/5"
                 />
                 <FormField
                   name="city"
-                  placeholder="Paris"
-                  label="Ville"
+                  placeholder={t(`cityPlaceholder`)}
+                  label={t(`cityLabel`)}
                   className="lg:w-2/5"
                 />
               </div>
             </div>
-            <Button className="lg:w-1/5">ENREGISTRER</Button>
+            <Button className="lg:w-1/5">{t(`buttonText`)}</Button>
           </div>
         </Form>
       </Formik>
