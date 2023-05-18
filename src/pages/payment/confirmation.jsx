2b3c2d5e66348a3793d48afa385a1ea0d8f5286a
William <wwautrin@hotmail.fr>
2023-05-18 13:24:09 +0200

    Trad payement & confirmation

--- src/pages/payment/confirmation.jsx
+++ src/pages/payment/confirmation.jsx
@@ -1,15 +1,25 @@
 import Button from "@/components/app/ui/Button"
 import { NavLink } from "@/components/utils/NavLink"
 import useAppContext from "@/web/hooks/useAppContext"
+import { serverSideTranslations } from "next-i18next/serverSideTranslations"
+import { useTranslation } from "next-i18next"
 import { useEffect } from "react"
 
 export const getServerSideProps = async (context) => {
   const { payment_intent, redirect_status } = context.query
+  const { locale } = context
+
+  const translations = await serverSideTranslations(locale, [
+    "confirmation",
+    "navbar",
+    "footer",
+  ])
 
   return {
     props: {
       payment_intent,
       redirect_status,
+      ...translations,
     },
   }
 }
@@ -32,14 +42,16 @@ const Confirmation = (props) => {
     }
   }, [payment_intent, redirect_status, cartItems, confirmOrder, clearCart])
 
+  const { t } = useTranslation("confirmation")
+
   return (
     <>
       <div className="flex justify-center items-center py-32">
         <div className="space-y-8 w-3/5 lg:w-1/4 lg:space-y-10">
-          <h1 className="font-bold text-xl">Commande effectuée !</h1>
-          <h2 className="font-bold text-lg">Merci pour votre achat.</h2>
+          <h1 className="font-bold text-xl">{t(`confirm`)}</h1>
+          <h2 className="font-bold text-lg">{t(`thanks`)}</h2>
           <p className="font-semibold">
-            Votre commande a bien été enregistrée sous le numéro
+            {t(`confirmNum`)}
             <span className="font-medium underline">
               {" "}
               {payment_intent.substring(3)}
@@ -51,7 +63,7 @@ const Confirmation = (props) => {
               className="my-6 bg-[#615043] hover:bg-[#927864] hover:cursor-pointer
              active:bg-[#615043] border border-black p-3.5 font-semibold rounded-md px-12"
             >
-              Continuer mes achats
+              {t(`buttonText`)}
             </Button>
           </NavLink>
         </div>
