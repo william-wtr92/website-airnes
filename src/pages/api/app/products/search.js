038641c1e9e0cb19e8f981e48b81a43f120bbbdb
William <wwautrin@hotmail.fr>
2023-05-19 10:40:04 +0200

    Trad of search page

--- src/pages/api/app/products/search.js
+++ src/pages/api/app/products/search.js
@@ -2,66 +2,70 @@ import ProductModel from "@/api/db/models/ProductModel"
 import validate from "@/api/middlewares/validate"
 import mw from "@/api/mw"
 import {
-    queryPageValidator, stringValidator,
+  queryPageValidator,
+  stringValidator,
 } from "@/components/validation/validation"
 import { NotFoundError } from "objection"
 
 const handler = mw({
-    GET: [
-        validate({
-            query: {
-                page: queryPageValidator.optional(),
-                search: stringValidator.optional(),
-            },
-        }),
-        async ({
-                   locals: {
-                       query: { search, page },
-                   },
-                   res,
-               }) => {
-            const limit = 18
-            page = parseInt(page, 10) || 1
-            const offset = (page - 1) * limit
+  GET: [
+    validate({
+      query: {
+        page: queryPageValidator.optional(),
+        search: stringValidator.optional(),
+      },
+    }),
+    async ({
+      locals: {
+        query: { search, page },
+      },
+      res,
+    }) => {
+      const limit = 18
+      page = parseInt(page, 10) || 1
+      const offset = (page - 1) * limit
 
-            const productsQuery = ProductModel.query()
-                .where("categoryId", "!=", 0)
-                .orderBy("id", "asc")
-                .limit(limit)
-                .offset(offset)
+      const productsQuery = ProductModel.query()
+        .where("categoryId", "!=", 0)
+        .orderBy("id", "asc")
+        .limit(limit)
+        .offset(offset)
 
-            if (search || search !== "") {
-                productsQuery.andWhere(function() {
-                    this.where("name", "like", `%${search}%`)
-                        .orWhere("description", "like", `%${search}%`)
-                })
-            }
+      if (search || search !== "") {
+        productsQuery.andWhere(function () {
+          this.where("name", "like", `%${search}%`).orWhere(
+            "description",
+            "like",
+            `%${search}%`
+          )
+        })
+      }
 
-            const products = await productsQuery
+      const products = await productsQuery
 
-            const totalCount = await ProductModel.query().count().first()
+      const totalCount = await ProductModel.query().count().first()
 
-            const pagination = {
-                page,
-                limit,
-                totalItems: parseInt(totalCount.count, 10),
-                totalPages: Math.ceil(totalCount.count / limit),
-            }
+      const pagination = {
+        page,
+        limit,
+        totalItems: parseInt(totalCount.count, 10),
+        totalPages: Math.ceil(totalCount.count / limit),
+      }
 
-            if (products) {
-                res.send({
-                    result: products,
-                    pagination: pagination,
-                })
+      if (products) {
+        res.send({
+          result: products,
+          pagination: pagination,
+        })
 
-                return
-            }
+        return
+      }
 
-            res.send({ result: "" })
+      res.send({ result: "" })
 
-            throw new NotFoundError()
-        },
-    ],
+      throw new NotFoundError()
+    },
+  ],
 })
 
 export default handler
--- src/pages/categories/search.jsx
+++ src/pages/categories/search.jsx
@@ -8,6 +8,7 @@ import ProductTemplate from "@/components/app/content/ProductTemplate"
 import Filters from "@/components/app/find/Filters"
 import { useRouter } from "next/router"
 import Pagination from "@/components/app/ui/Pagination"
+import { useTranslation } from "next-i18next"
 
 export const getServerSideProps = async (context) => {
   const { locale } = context
@@ -17,22 +18,18 @@ export const getServerSideProps = async (context) => {
 
   const [products, materialsAndCategories] = await Promise.all([
     axios.get(
-        `${
-            config.path
-        }api${routes.api.app.products.searchProducts()}?page=${pageQuery}&search=${search}`
+      `${
+        config.path
+      }api${routes.api.app.products.searchProducts()}?page=${pageQuery}&search=${search}`
     ),
     axios.get(
-        `${config.path}api${routes.api.admin.materials.getMaterialsAndCategory()}`
+      `${config.path}api${routes.api.admin.materials.getMaterialsAndCategory()}`
     ),
   ])
 
   return {
     props: {
-      ...(await serverSideTranslations(locale, [
-        "products",
-        "navbar",
-        "footer",
-      ])),
+      ...(await serverSideTranslations(locale, ["search", "navbar", "footer"])),
       products: products.data.result,
       pagination: products.data.pagination,
       categories: materialsAndCategories.data.categories,
@@ -63,8 +60,8 @@ const SearchPage = (props) => {
 
   useEffect(() => {
     if (
-        debouncedSearchQuery !== "" &&
-        debouncedSearchQuery !== router.query.searchQuery
+      debouncedSearchQuery !== "" &&
+      debouncedSearchQuery !== router.query.searchQuery
     ) {
       router.push({
         pathname: router.pathname,
@@ -77,116 +74,118 @@ const SearchPage = (props) => {
     setFilterShow(!filterShow)
   }
 
+  const { t } = useTranslation("search")
+
   return (
-      <>
-        <div className="flex flex-rows">
-          <div
-              className={`${
-                  filterShow ? `block ` : `hidden`
-              } flex flex-col border-r-2 p-4 fixed inset-0 top-[3.8125rem] left-[max(0px,calc(50%-45rem))] right-auto w-full md:w-[36%] pb-10 overflow-y-auto`}
-          >
-            <div className="flex justify-between pb-4">
-              <button className="underline text-xl" onClick={handleShowFilter}>
-                Réintialiser
-              </button>
-              <button className="underline text-xl" onClick={handleShowFilter}>
-                Fermer
-              </button>
-            </div>
-            <div className={`flex flex-col lg:flex-row justify-between mt-2`}>
-              <div className="flex flex-col">
-                <div className="font-bold pb-2 text-2xl text-black">
-                  Prix min €
-                </div>
-                <input
-                    className={` border rounded-full border-black bg-[#EDE5E0] text-black placeholder-[#443021] p-4 px-4 md:px-2 md:p-2 lg:px-4 lg:p-4`}
-                    type="search"
-                    placeholder=". . . €"
-                />
+    <>
+      <div className="flex flex-rows">
+        <div
+          className={`${
+            filterShow ? `block ` : `hidden`
+          } flex flex-col border-r-2 p-4 fixed inset-0 top-[3.8125rem] left-[max(0px,calc(50%-45rem))] right-auto w-full md:w-[36%] pb-10 overflow-y-auto`}
+        >
+          <div className="flex justify-between pb-4">
+            <button className="underline text-xl" onClick={handleShowFilter}>
+              {t(`reset`)}
+            </button>
+            <button className="underline text-xl" onClick={handleShowFilter}>
+              {t(`close`)}
+            </button>
+          </div>
+          <div className={`flex flex-col lg:flex-row justify-between mt-2`}>
+            <div className="flex flex-col">
+              <div className="font-bold pb-2 text-2xl text-black">
+                {t(`min`)}
               </div>
-              <div className="flex flex-col">
-                <div className="font-bold pb-2 text-2xl text-black">
-                  Prix max €
-                </div>
-                <input
-                    className={`border rounded-full border-black bg-[#EDE5E0] text-black placeholder-[#443021] p-4 px-4 md:px-2 md:p-2 lg:p-4 lg:px-4`}
-                    type="search"
-                    placeholder=". . . €"
-                />
-              </div>
+              <input
+                className={` border rounded-full border-black bg-[#EDE5E0] text-black placeholder-[#443021] p-4 px-4 md:px-2 md:p-2 lg:px-4 lg:p-4`}
+                type="search"
+                placeholder={t(`minPlaceholder`)}
+              />
             </div>
-            <div
-                id={"filtres"}
-                className="flex flex-col gap-6 relative leading-6"
-            >
-              <Filters data={categories} name={"Catégories"} />
-              <Filters data={materials} name={"Matériaux"} />
+            <div className="flex flex-col">
+              <div className="font-bold pb-2 text-2xl text-black">
+                {t(`max`)}
+              </div>
+              <input
+                className={`border rounded-full border-black bg-[#EDE5E0] text-black placeholder-[#443021] p-4 px-4 md:px-2 md:p-2 lg:p-4 lg:px-4`}
+                type="search"
+                placeholder={t(`maxPlaceholder`)}
+              />
             </div>
           </div>
           <div
-              className={`${
-                  filterShow
-                      ? `hidden md:ml-[36%] md:block w-full `
-                      : `block mx-auto w-full`
-              } gap-4 flex flex-col justify-center mx-6 mt-20 pb-10 px-6  lg:mt-8`}
+            id={"filtres"}
+            className="flex flex-col gap-6 relative leading-6"
           >
-            <div className="text-center text-3xl text-black font-bold pb-6">
-              Recherche
-            </div>
-            <div className="flex justify-center gap-4">
-              <div className="flex justify-center gap-4 ">
-                <AdjustmentsVerticalIcon
-                    className=" h-10 w-10 color-[#615043]"
-                    onClick={handleShowFilter}
-                />
-                <div className="hidden md:block text-xl flex-none text-center">
-                  Filtrer
-                </div>
-                <div className="flex-1">
-                  <input
-                      className={`pl-6 md:pr-[35%] border border-gray-500 bg-transparent text-black placeholder-[#443021] py-2`}
-                      type="search"
-                      placeholder="Rechercher"
-                      onChange={(e) => setSearchQuery(e.target.value)}
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="text-center text-3xl font-bold text-black mt-5">
-              Résultat
-            </div>
-            <div className="text-center flex justify-center gap-2 mt-5">
-              <FunnelIcon className="flex-none h-10 w-10 color-[#615043]" />
-              Trier par : (asc)
-            </div>
-            <div className="flex flex-col items-center">
-              <div
-                  className={`${
-                      filterShow ? `lg:grid-cols-2` : `lg:grid-cols-3`
-                  } w-5/6 grid gap-8 grid-cols-1 md:grid-cols-2 mb-20 mt-10 `}
-              >
-                {products.map((product) => (
-                    <ProductTemplate key={product.id} product={product} />
-                ))}
-              </div>
-              {products.length === 0 && <div>AUNCUN RÉSULTAT</div>}
-              {query.pageQuery === "1" ? (
-                  products.length === 18 && (
-                      <Pagination
-                          totalPages={pagination.totalPages}
-                          currentPage={pagination.page}
-                      />
-                  )
-              ) : (
-                  <Pagination
-                      totalPages={pagination.totalPages}
-                      currentPage={pagination.page}
-                  />
-              )}
-            </div>
+            <Filters data={categories} name={"Catégories"} />
+            <Filters data={materials} name={"Matériaux"} />
           </div>
         </div>
-      </>
+        <div
+          className={`${
+            filterShow
+              ? `hidden md:ml-[36%] md:block w-full `
+              : `block mx-auto w-full`
+          } gap-4 flex flex-col justify-center mx-6 mt-20 pb-10 px-6  lg:mt-8`}
+        >
+          <div className="text-center text-3xl text-black font-bold pb-6">
+            {t(`search`)}
+          </div>
+          <div className="flex justify-center gap-4">
+            <div className="flex justify-center gap-4 ">
+              <AdjustmentsVerticalIcon
+                className=" h-10 w-10 color-[#615043]"
+                onClick={handleShowFilter}
+              />
+              <div className="hidden md:block text-xl flex-none text-center">
+                {t(`filter`)}
+              </div>
+              <div className="flex-1">
+                <input
+                  className={`pl-6 md:pr-[35%] border border-gray-500 bg-transparent text-black placeholder-[#443021] py-2`}
+                  type="search"
+                  placeholder={t(`searchPlaceholder`)}
+                  onChange={(e) => setSearchQuery(e.target.value)}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="text-center text-3xl font-bold text-black mt-5">
+            {t(`result`)}
+          </div>
+          <div className="text-center flex justify-center gap-2 mt-5">
+            <FunnelIcon className="flex-none h-10 w-10 color-[#615043]" />
+            {t(`asc`)}
+          </div>
+          <div className="flex flex-col items-center">
+            <div
+              className={`${
+                filterShow ? `lg:grid-cols-2` : `lg:grid-cols-3`
+              } w-5/6 grid gap-8 grid-cols-1 md:grid-cols-2 mb-20 mt-10 `}
+            >
+              {products.map((product) => (
+                <ProductTemplate key={product.id} product={product} />
+              ))}
+            </div>
+            {products.length === 0 && <div>{t(`noresult`)}</div>}
+            {query.pageQuery === "1" ? (
+              products.length === 18 && (
+                <Pagination
+                  totalPages={pagination.totalPages}
+                  currentPage={pagination.page}
+                />
+              )
+            ) : (
+              <Pagination
+                totalPages={pagination.totalPages}
+                currentPage={pagination.page}
+              />
+            )}
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 export default SearchPage
