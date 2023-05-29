337f904512da0a68af0caf258b86ab102929709d
William <wwautrin@hotmail.fr>
2023-05-29 18:58:02 +0200

    format code

--- src/pages/api/app/products/search.js
+++ src/pages/api/app/products/search.js
@@ -2,46 +2,57 @@ import ProductModel from "@/api/db/models/ProductModel"
 import validate from "@/api/middlewares/validate"
 import mw from "@/api/mw"
 import {
-    boolValidator, numberValidator,
-    queryPageValidator, stringValidator,
+  boolValidator,
+  numberValidator,
+  queryPageValidator,
+  stringValidator,
 } from "@/components/validation/validation"
 import { NotFoundError } from "objection"
 
 const handler = mw({
-    GET: [
-        validate({
-            query: {
-                page: queryPageValidator.optional(),
-                search: stringValidator.optional(),
-                promo: boolValidator.optional(),
-                stock: boolValidator.optional(),
-                category: numberValidator.optional(),
-                material: numberValidator.optional(),
-                order: stringValidator.optional(),
-                minPrice: numberValidator.optional(),
-                maxPrice: numberValidator.optional()
-            },
-        }),
-        async ({
-                   locals: {
-                       query: { search, page, promo, stock, category, material, order, minPrice, maxPrice },
-                   },
-                   res,
-               }) => {
-            const limit = 18
-            page = parseInt(page, 10) || 1
-            const offset = (page - 1) * limit
+  GET: [
+    validate({
+      query: {
+        page: queryPageValidator.optional(),
+        search: stringValidator.optional(),
+        promo: boolValidator.optional(),
+        stock: boolValidator.optional(),
+        category: numberValidator.optional(),
+        material: numberValidator.optional(),
+        order: stringValidator.optional(),
+        minPrice: numberValidator.optional(),
+        maxPrice: numberValidator.optional(),
+      },
+    }),
+    async ({
+      locals: {
+        query: {
+          search,
+          page,
+          promo,
+          stock,
+          category,
+          material,
+          order,
+          minPrice,
+          maxPrice,
+        },
+      },
+      res,
+    }) => {
+      const limit = 18
+      page = parseInt(page, 10) || 1
+      const offset = (page - 1) * limit
 
-            const orderColumn = order? "price" : "id"
-            const orderBy = order || "asc"
+      const orderColumn = order ? "price" : "id"
+      const orderBy = order || "asc"
 
+      const productsQuery = ProductModel.query()
+        .where("categoryId", "!=", 0)
+        .orderBy(orderColumn, orderBy)
+        .limit(limit)
+        .offset(offset)
 
-            const productsQuery = ProductModel.query()
-                .where("categoryId", "!=", 0)
-                .orderBy(orderColumn, orderBy)
-                .limit(limit)
-                .offset(offset)
-            
       if (search || search !== "") {
         productsQuery.andWhere(function () {
           this.where("name", "like", `%${search}%`).orWhere(
@@ -51,33 +62,32 @@ const handler = mw({
           )
         })
       }
-          
-            if (promo === true) {
-                productsQuery.where("promotion", "!=", 0)
-            }
 
-            if (stock === true) {
-                productsQuery.where("quantity", "!=", 0)
-            }
+      if (promo === true) {
+        productsQuery.where("promotion", "!=", 0)
+      }
 
-            if(category && category !== 0) {
-                productsQuery.where("categoryId", "=", category)
-            }
+      if (stock === true) {
+        productsQuery.where("quantity", "!=", 0)
+      }
 
-            if(material && material !== 0) {
-                productsQuery.where("materialId", "=", material)
-            }
+      if (category && category !== 0) {
+        productsQuery.where("categoryId", "=", category)
+      }
 
-            if(minPrice && minPrice !== 0) {
-                productsQuery.where("price",">=", minPrice)
-            }
+      if (material && material !== 0) {
+        productsQuery.where("materialId", "=", material)
+      }
 
-            if(maxPrice && maxPrice !== 0) {
-                productsQuery.where("price", "<=", maxPrice)
-            }
+      if (minPrice && minPrice !== 0) {
+        productsQuery.where("price", ">=", minPrice)
+      }
 
+      if (maxPrice && maxPrice !== 0) {
+        productsQuery.where("price", "<=", maxPrice)
+      }
 
-            const products = await productsQuery
+      const products = await productsQuery
 
       const totalCount = await ProductModel.query().count().first()
 
--- src/web/services/app/products/getFilter.js
+++ src/web/services/app/products/getFilter.js
@@ -1,19 +1,17 @@
 import routes from "@/web/routes"
 
 const getFilterServices =
-    ({ api }) =>
-        async () => {
-            try {
-                const { data } = await api.get(
-                    routes.api.app.products.getFilter()
-                )
+  ({ api }) =>
+  async () => {
+    try {
+      const { data } = await api.get(routes.api.app.products.getFilter())
 
-                return [null, data]
-            } catch (err) {
-                const error = err.response?.data?.error || "Oops. Something went wrong"
+      return [null, data]
+    } catch (err) {
+      const error = err.response?.data?.error || "Oops. Something went wrong"
 
-                return [error]
-            }
-        }
+      return [error]
+    }
+  }
 
 export default getFilterServices
