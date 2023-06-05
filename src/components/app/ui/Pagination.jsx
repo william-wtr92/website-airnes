3466f86860bd605a5e3acc951d9fa8e1b3873f67
Charlotte <charlotte.waegeneire@hotmail.fr>
2023-06-05 10:57:09 +0200

    refactor pagination

--- src/components/app/ui/Pagination.jsx
+++ src/components/app/ui/Pagination.jsx
@@ -15,12 +15,11 @@ const Pagination = (props) => {
   const previousPage = Math.max(1, currentPage - 1)
   const nextPage = Math.min(totalPages, currentPage + 1)
 
+  const middlePage = totalPages > 2 && (currentPage !== 1 && currentPage !== totalPages) ? currentPage : "..."
   const pages = [
     1,
-    totalPages > 2 ? 2 : null,
-    currentPage > 2 && currentPage < totalPages - 1 ? currentPage : "...",
-    totalPages > 3 ? totalPages - 1 : null,
-    totalPages > 1 ? totalPages : null,
+    totalPages > 2 ? middlePage : null,
+    totalPages
   ].filter(Boolean)
 
   const handlePageClick = (e, page) => {
