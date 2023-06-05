337b4ddabdcc0d48b0fe610d708c21d346390a35
William <wwautrin@hotmail.fr>
2023-06-05 21:19:05 +0200

    trad

--- src/pages/user/add.jsx
+++ src/pages/user/add.jsx
@@ -1,4 +1,26 @@
 import AddAddressForm from "@/components/app/content/AddAddress"
+import { getAuthorization } from "@/web/helper/getAuthorization"
+import { serverSideTranslations } from "next-i18next/serverSideTranslations"
+
+export const getServerSideProps = async (context) => {
+  const { req, query, locale } = context
+
+  const redirect = getAuthorization("user", req, query)
+
+  if (redirect) {
+    return redirect
+  }
+
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, [
+        "addaddress",
+        "navbar",
+        "footer",
+      ])),
+    },
+  }
+}
 
 const AddAddress = () => {
   return (
